c3630752f340b1c859f9755d5b424c430335e26a
yasemingunal <76755092+yasemingunal@users.noreply.github.com>
2022-11-12 19:18:55 -0500

    Add files via upload

---
+++ js/amsvlog.js
@@ -0,0 +1,15 @@
+// var amsvlog = document.querySelector("#ams_video_1");
+
+// window.addEventListener("load", function(){
+//     console.log("Good job opening the window");
+//     amsvlog.autoplay=false;
+//     amsvlog.loop=false;
+//     console.log("Setup is complete");
+// });
+
+// document.querySelector("#play").addEventListener("click", function() { 
+//     console.log("play video");
+//     amsvlog.play();
+// })
+
+document.getElementById('contact-message').style.height="75px";
